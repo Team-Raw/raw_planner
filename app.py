ce9c5c67e83c22da6945348f6d446d215ada7320
Minhyeok Kang <santa4246@naver.com>
2023-11-27 00:52:03 +0900

    [frontend] index.html 추가, link 연결

--- app.py
+++ app.py
@@ -9,10 +9,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# 구현필요
-# @app.route('/service',methods=('GET', 'POST'))
-# def service():
-#     return render_template('service.html')
+@app.route('/service',methods=('GET', 'POST'))
+def service():
+    return render_template('service.html')
 
 @app.route('/search',methods=('GET', 'POST'))
 def search():
